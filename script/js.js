abf9d4085da255ec2f89ad7f8889fa94e850b8a7
formyprojects <formyproject@bigmir.net>
2018-04-11 11:31:30 +0300

    first page

---
+++ script/js.js
@@ -0,0 +1,11 @@
+function showText(el) {
+	// body...
+	if (el.previousElementSibling.clientHeight === 80) {
+		el.previousElementSibling.style.height = "100%";
+		el.innerHTML = "ShowLess...";
+	}
+	else {
+		el.previousElementSibling.style.height = "80px";
+		el.innerHTML = "Read More ...";
+	}
+}
